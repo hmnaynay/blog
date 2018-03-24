35c1ad14e822f0b60e9224f4072da91e251fae95
Naomi Smith <naomi@devthe.com>
2018-03-24 19:32:15 +0000

    Added new post route.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models import Post
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.jinja', posts=Post.get_all())
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'GET':
+        return render_template('new.jinja')
+
+    # Get data from request.
+    title = request.form['title']
+    content = request.form['content']
+
+    Post.new(title, content)
+
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
